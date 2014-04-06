75469be25a1b4b2af613b6f7e764e2025f29a781
Larry Gilbert <larry@l2g.to>
2014-04-05 18:35:36 -0700

    Use true shell script instead of make file for stage 1

---
+++ provisioners/stage1/3_1_introduction.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+wget http://www.linuxfromscratch.org/lfs/view/stable/wget-list
+wget http://www.linuxfromscratch.org/lfs/view/stable/md5sums
+
+mkdir -v ${LFS}/sources
+chmod -v a+wt ${LFS}/sources
+wget -i wget-list -P ${LFS}/sources
+cp md5sums ${LFS}/sources
+# pushd ${LFS}/sources
+# md5sum -c md5sums
+# popd
